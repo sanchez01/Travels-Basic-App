da53635c8fb34249e1a4bbaf7f8c60be0cdb06a4
ManuHernandez <HernandezManuel370@gmail.com>
2017-05-04 20:53:01 -0400

    First Commit.

---
+++ Travel.Messaging/Commands/IRegisterTravelCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Travel.Messaging.Commands
+{
+    public interface IRegisterTravelCommand
+    {
+        string FirstName { get;  }
+        string LastName { get;  }
+        string NatureTravel { get;  }
+        string Country { get;  }
+        string City { get;  }
+        string State { get;  }
+        DateTime From { get;  }
+        DateTime To { get;  }
+        string Address { get;  }
+        string Hotel { get; }
+        string ContactNumber { get;  }
+    }
+}
---
+++ Travel.Utils/Constants.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Travel.Utils
+{
+    public class Constants
+    {
+        public static string RabbitMqUri { get; private set; } = "rabbitmq://localhost/Travels";
+        public static string Username { get; private set; } = "guest";
+        public static string Password { get; private set; } = "guest";
+    }
+}
